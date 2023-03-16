fb1bfb58303caf74a2e3f426db0cf1e0e30842f2
Chad Tennent <stealthbos@protonmail.com>
2023-03-15 21:00:51 -0700

    refactoring into smaller components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,63 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { onMount } from "svelte";
+  import "@picocss/pico";
+  import AccuWeatherClient from "accu-weather-api-wrapper";
+
+  let currentWeather;
+  let currentTemp: number;
+  let location = "Portland, OR";
+  let description: string;
+  let icon;
+  let getWeather = async () => {
+    const weather = new AccuWeatherClient({
+      apikey: "sbewALxc4hIHOgvHhG9kDWrsOKfpLoO7",
+    });
+    //await weather.location.citySearch("Portland, or").then(console.log);
+    // await weather.forecast.getDailyForecast(1, "350473").then(console.log);
+    currentWeather = await weather.currentConditions.currentCondition("350473");
+
+    console.log(currentWeather);
+
+    currentTemp = currentWeather[0].Temperature.Imperial.Value;
+    description = currentWeather[0].WeatherText;
+    icon = `https://www.accuweather.com/images/weathericons/${currentWeather[0].WeatherIcon}.svg`;
+  };
+
+  onMount(() => getWeather());
+
+  //TODO: detect location via ip address to get current weather?
+  //TODO: Fix the icon.
+  //TODO: Components: 'Current Weather' card (mvp on this page), '5 day forecast' card (5 smaller daily cards?)
+</script>
+
+<main class="container-fluid">
+  <article>
+    <header><h3>Weather in {location}</h3></header>
+    <div class="container">
+      <div class="outer">
+        <img src={icon} style="padding-right:10px;" />
+        <div class="inner">
+          <h2>{currentTemp}°f</h2>
+          <h3>{description}</h3>
+        </div>
+      </div>
+    </div>
+  </article>
+</main>
+
+<style>
+  .outer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    /* border: 1px solid black; */
+    text-align: right;
+  }
+  .inner {
+    display: grid;
+    text-align: center;
+  }
+  header {
+    text-align: center;
+  }
+</style>
---
+++ src/routes/currentWeather.svelte
@@ -0,0 +1,38 @@
+<script>
+  export let location;
+  export let icon;
+  export let currentTemp;
+  export let description;
+</script>
+
+<main class="container-fluid">
+  <article>
+    <header><h3>Weather in {location}</h3></header>
+    <div class="container">
+      <div class="outer">
+        <img src={icon} style="padding-right:10px;" />
+        <div class="inner">
+          <h2>{currentTemp}°f</h2>
+          <h3>{description}</h3>
+        </div>
+      </div>
+    </div>
+  </article>
+</main>
+
+<style>
+  .outer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    /* border: 1px solid black; */
+    text-align: right;
+  }
+  .inner {
+    display: grid;
+    text-align: center;
+  }
+  header {
+    text-align: center;
+  }
+</style>
